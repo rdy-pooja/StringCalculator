ae76132f6580075573f359f7b2fedfc3affb3e3d
Pooja <rdy.pooja@gmail.com>
2022-04-13 12:52:57 +0530

    Updated code changes

---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorTDD
+{
+    public class StringCalculator
+    {
+        private static List<char> delims = new List<char>() { ',', '\n' };
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add("//;\n3;5;7;8;0"));
+            Console.ReadKey();
+        }
+
+        public static int Add(string numbers)
+        {
+            if(IsEmptyString(numbers) == 0)
+            {
+                return 0;
+            }
+            AddNewDelimiters(ref numbers);
+            return ExtractNumbers(numbers);
+        }
+
+        //Checking if the string is empty or not.
+        public static int IsEmptyString(string numbers)
+        {
+            if (numbers.Equals(""))
+            {
+                return 0;
+            }
+            return 1;
+        }
+
+        //Extract only numbers eliminate all delimiters.
+        public static int ExtractNumbers(string numbers)
+        {
+            int count = 0;
+            string[] arr = numbers.Split(delims.ToArray());
+            foreach(string s in arr)
+            {
+                IsNegativeNumber(Convert.ToInt32(s));
+                count += Convert.ToInt32(s);
+            }
+            return count;
+        }
+
+        //Handle new delimiters and add it to the list.
+        public static void AddNewDelimiters(ref string numbers)
+        {
+            if(numbers[2] == ',' || numbers[2] == '\n')
+            {
+                throw new Exception("Delimiter already Exists");
+            }
+            if(numbers[0] == '/' && numbers[1] == '/' && numbers[3] == '\n')
+            {
+                delims.Add(numbers[2]);
+                numbers = numbers.Remove(0, 4);
+            }
+        }
+
+        //Handle negative numbers.
+        public static void IsNegativeNumber(int num)
+        {
+            if(num<0)
+            {
+                throw new Exception("Negatives not allowed.");
+            }
+        }
+    }
+}
---
+++ StringCalculatorTest/StringCalculatorTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using StringCalculatorTDD;
+
+namespace StringCalculatorTest
+{
+    public class StringCalculatorTest
+    {
+        [Test]
+        public void TestForEmptyString()
+        {
+            Assert.AreEqual(0, StringCalculator.IsEmptyString(""));
+        }
+        [Test]
+        public void TestForExtractNumbers()
+        {
+            Assert.AreEqual(23, StringCalculator.Add("3,5,7,8,0"));
+        }
+        [Test]
+        public void TestHandleNewLineDelimiter()
+        {
+            Assert.AreEqual(6, StringCalculator.Add("1\n2,3"));
+        }
+        [Test]
+        public void TestForHandlingNewDelimiter()
+        {
+            Assert.AreEqual(25, StringCalculator.Add("//;\n3;5;7;8;2"));
+        }
+        [Test]
+        public void TestForNegativeNumbers()
+        {
+            var ex = Assert.Throws<Exception>(() => StringCalculator.Add("3,-2,5,6"));
+            Assert.AreEqual("Negatives not allowed.", ex.Message);
+        }
+        [Test]
+        public void TestAlreadyExisitingDelimiter()
+        {
+            var ex = Assert.Throws<Exception>(() => StringCalculator.Add("//,\n3;5;7;8;2"));
+            Assert.AreEqual("Delimiter already Exists", ex.Message);
+        }
+    }
+}
